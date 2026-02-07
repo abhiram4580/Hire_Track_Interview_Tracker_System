56999ddcba59b6820d34e0e042726925429c3c22
Abhiram Naidu <yalla.21ug@iiitranchi.ac.in>
2026-02-07 18:56:09 +0530

    Update frontend Apollo Client to use environment variable for API URL

--- frontend/lib/apolloClient.ts
+++ frontend/lib/apolloClient.ts
@@ -8,7 +8,7 @@ import { setContext } from "@apollo/client/link/context";
 import { onError } from "@apollo/client/link/error";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/graphql",
 });
 
 const authLink = setContext(() => {
