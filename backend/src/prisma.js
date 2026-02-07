756bdd32ac2bc8a48c1c67f963ae8c74c74ee298
Abhiram Naidu <yalla.21ug@iiitranchi.ac.in>
2026-02-07 18:06:10 +0530

    Downgrade Prisma to v5 and remove adapter to fix Render deployment

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,17 +1,11 @@
 require("dotenv").config();
 
 const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
-const { Pool } = require("pg");
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
